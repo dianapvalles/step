e1939e03146feac8aeae95e4597bf14f7b36b72f
Diana Valles <dianapaolavalles@gmail.com>
2020-06-18 22:53:34 +0000

    Add Google Map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -66,3 +66,8 @@ function deleteComment(text){
     params.append('id',text.id);
     fetch('/delete-data', {method: 'POST', body: params});
 }
+
+/** Creates a map and adds it to the page. */
+function createMap(){
+    const map = new google.maps.Map(document.getElementById('map'), {center: {lat: 37.422, lng: -122.084}, zoom:16});
+}
