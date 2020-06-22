6d6d2f088dd8240049a29bb211602554bfb5b322
Diana Valles <dianapaolavalles@gmail.com>
2020-06-22 16:01:18 +0000

    Use POST requests to allow users to add markers to the map, added object class Marker and created its servlet to store and fetch markers to a from Datastore

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+let map;
+/** Editable marker that displays when a user clicks in the map */
+let editMarker;
+
 /**
  * Adds a random facts to the page.
  */
@@ -69,7 +73,7 @@ function deleteComment(commentJson){
 
 /** Creates a map and adds it to the page. */
 function createMap(){
-    const map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 37.422, lng: -122.084 }, 
         zoom:10,
         // Enables dark mode view
@@ -157,18 +161,6 @@ function createMap(){
 
     map.setTilt(45);
 
-    var markerMexico = new google.maps.Marker({
-        position: {lat: 20.5809134, lng: -87.1218867},
-        map: map,
-        title: 'Click'
-    });
-
-    var markerChicago = new google.maps.Marker({
-        position: {lat: 41.8028818, lng: -87.5852623},
-        map: map,
-        title: 'Click'
-    });
-
     var contentMexico = '<div id="info-window">'+
         '<h1>Xcaret Mexico</h1>'+
         '<p><b>Xcaret</b> is one of greatest nature parks. '+
@@ -185,16 +177,89 @@ function createMap(){
         '<a href="https://www.msichicago.org/">Attribution: Museum of Science and Industry</a>'+
         '</div>';
 
-    addInfoWindow(map,contentMexico,markerMexico);
-    addInfoWindow(map,contentChicago,markerChicago);
+    var markerMexico = createMarkerForDisplay(20.5809134,-87.1218867,contentMexico);
+    var markerChicago = createMarkerForDisplay( 41.8028818,-87.5852623, contentChicago);
+
+    /** When the user clicks in the map, show a marker with a text box the user can edit. */
+    map.addListener('click',(event) => {
+        createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+    });
+
+    fetchMarkers();
 }
 
-function addInfoWindow(map,contentForWindow,marker){
-    var infowindow = new google.maps.InfoWindow({
-        content: contentForWindow
+/** Fetches markers from the backend and adds them to the map. */
+function fetchMarkers(){
+    fetch('/markers').then(response => response.json()).then((markers) =>{
+        markers.forEach((marker) => {
+            createMarkerForDisplay(marker.lat, marker.lng, marker.content);
+        });
     });
+}
 
-    marker.addListener('click',function(){
+/** Creates a marker that shows a read-only info window when clicked. */
+function createMarkerForDisplay(lat, lng, content) {
+  const marker = 
+    new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+
+  var infowindow = new google.maps.InfoWindow({
+        content: content
+  });
+
+  marker.addListener('click',() => {
         infowindow.open(map, marker);
-    });
+  });
+}
+
+/** Sends a marker to the backend for saving. */
+function postMarker(lat, lng, content) {
+  const params = new URLSearchParams();
+  params.append('lat', lat);
+  params.append('lng', lng);
+  params.append('content', content);
+
+  fetch('/markers', {method: 'POST', body: params});
+}
+
+/** Creates a marker that shows a textbox the user can edit. */
+function createMarkerForEdit(lat, lng) {
+  // If we're already showing an editable marker, then remove it.
+  if (editMarker) {
+    editMarker.setMap(null);
+  }
+  
+  editMarker =
+      new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+
+  let infoWindow = new google.maps.InfoWindow({content:buildInfoWindowInput(lat, lng)});
+
+  // When the user closes the editable info window, remove the marker.
+  google.maps.event.addListener(infoWindow, 'closeclick', () => {
+    editMarker.setMap(null);
+  });
+
+  infoWindow.open(map, editMarker);
+}
+
+/**
+ * Builds and returns HTML elements that show an editable textbox and a submit
+ * button.
+ */
+function buildInfoWindowInput(lat, lng) {
+  const textBox = document.createElement('textarea');
+  const button = document.createElement('button');
+  button.appendChild(document.createTextNode('Submit'));
+
+  button.onclick = () => {
+    postMarker(lat, lng, textBox.value);
+    createMarkerForDisplay(lat, lng, textBox.value);
+    editMarker.setMap(null);
+  };
+
+  const containerDiv = document.createElement('div');
+  containerDiv.appendChild(textBox);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(button);
+
+  return containerDiv;
 }
